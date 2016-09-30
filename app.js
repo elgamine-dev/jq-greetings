3938110b8f3841cbcd8e2d1638c0163b3c614692
axel <axel.bouillart@gmail.com>
2016-09-30 11:45:12 +0200

    jquery data recup

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('have fun !');
+$("button").click(function(){
+
+	var first_name = $("#first_name").val();
+	var city = $("#city").val();
+	var last_name = $("#last_name").val();
+	console.log(first_name);
+
+	var gerard = {
+
+		first_name : first_name,
+		city : city,
+		last_name : last_name
+
+	}
+	console.log(gerard);
+	$("span").html(gerard.first_name +" "+last_name)
+
+	
+
+});
+
